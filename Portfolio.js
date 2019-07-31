03e77005df0f725707bc612d58a0e550c1bf2514
Jordan R <rjordan7@vt.edu>
2019-07-31 15:32:57 -0400

    js alterations

--- Portfolio.js
+++ Portfolio.js
@@ -1,5 +1,5 @@
   $(document).ready(function () {
     $(window).scroll(function(){
-      $("#welcome-section").css("opacity", 1 - $(window).scrollTop() / 450);
+      $("#welcome-section").css("opacity", 1 - $(window).scrollTop() / 600);
     });
 });
